950e7dba45861f3358583d9abcbd583ece5c17ef
Zee Mudia <iosazee1@gmail.com>
2023-10-19 07:22:55 +0100

    initial commit

---
+++ EekiBooksOnline/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EekiBooksOnline.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EekiBooksOnline.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
