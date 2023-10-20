bd642bc98d57039d508e10edb938c584c5afe0bd
Zee Mudia <iosazee1@gmail.com>
2023-10-20 01:09:27 +0100

    creating categories of books functionality

--- EekiBooksOnline/Controllers/CategoryController.cs
+++ EekiBooksOnline/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EekiBooksOnline.Data;
+using EekiBooksOnline.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EekiBooksOnline.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
         }
     }
 }
